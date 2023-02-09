4b73d02fe194d69859f8d3a208cbda615df542fb
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-02-09 12:36:17 -0600

    Logout component

---
+++ public/js/logout.js
@@ -0,0 +1,21 @@
+// Logout component
+const logoutBtn = document.querySelector('#logout-button');
+
+
+logoutButton.addEventListener('click', () => {
+    fetch('/api/logout', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+      console.log('Success! Logged out', data);
+      localStorage.removeItem('user');
+    })
+      .catch(error => {
+      console.error('Error: Unable to logout:', error);
+  });
+
+});
