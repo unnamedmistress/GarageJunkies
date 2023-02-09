806a36182cfb36ecfd4371aa7b33120ed14cad69
Jacob Studstill <jacob.studstill@gmail.com>
2023-02-09 12:52:20 -0500

    Our handlebars aren't working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,26 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Project } = require('../../models');
+
+router.get ('/', async (req,res) =>{
+    try {
+        const users = await User.findAll({
+            //Not sure what to include here
+          include:[{
+            model:Project
+        }]
+        })
+        res.status(200).json(users)
+        console.log(users)
+      }
+      catch (err) {
+        res.status(500).json(err)
+        console.log(err)
+      }
+
+
+
+})
+
 
 router.post('/', async (req, res) => {
   try {
