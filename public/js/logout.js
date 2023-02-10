6b1b91c2b2d47d9b6c805cf501dee49b701ede5c
Jacob Studstill <jacob.studstill@gmail.com>
2023-02-10 11:11:03 -0500

    Fixed the functionality of the logout button

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,19 +1,20 @@
 
 // Logout component
-const logoutBtn = document.querySelector('#logout-button');
+const logoutBtn = document.querySelector('#logout');
 
 
 logoutBtn.addEventListener('click', () => {
-    fetch('/api/logout', {
+    fetch('/api/users/logout', {
       method: 'POST',
       headers: {
       'Content-Type': 'application/json'
       },
     })
-      .then(res => res.json())
+      // .then(res => res.json())
       .then(data => {
       console.log('Success! Logged out', data);
       localStorage.removeItem('user');
+      window.location.href = '/login';
     })
       .catch(error => {
       console.error('Error: Unable to logout:', error);
