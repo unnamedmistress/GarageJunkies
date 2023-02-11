2bcc700455df6f32b0bb1309277e54c7fb383e8a
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-02-11 12:12:03 -0600

    Moved search route for separation of concerns

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const projectRoutes = require('./project-routes');
-const listingRoutes = require('./listing-routes');
+const searchRoutes = require('./search-routes');
+
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
-router.use('/listings', listingRoutes);
+router.use('/search', searchRoutes);
 
 module.exports = router;
