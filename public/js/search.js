d60ff9b90a2fba21ec8df3561814139707302497
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-02-11 20:03:58 -0600

    Marker on map letsgooooo

--- public/js/search.js
+++ public/js/search.js
@@ -11,23 +11,20 @@ search.addEventListener('submit', async (e) => {
     // Fetch to backend to get listing data
     const res = await fetch(`/api/search/${zip}?zipCode=${zip}`);
     const listings = await res.json();
-      return(listings);
+    // Init the map
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: { lat: listings[0].latitude, lng: listings[0].longitude }
+  });
+    listings.forEach((listing) => {
+      const marker = new google.maps.Marker({
+        position: { lat: listing.latitude, lng: listing.longitude },
+        map,
+        title: listing.address,
+      });
+    });
   } catch (err) {
     console.error(err);
   }
 });
 
-
-
-// // Place markers on map for each listing
-// async function displayListings(zipCode, item) {
-//   const listings = await getListings(zipCode, item);
-
-//   // Create marker for each listing
-//   listings.forEach(listing => {
-//     const marker = new google.maps.Marker({
-//       position: { lat: listing.latitude, lng: listing.longitude },
-//       map: map
-//     });
-//   });
-// }
