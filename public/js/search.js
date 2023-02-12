ed92cc68151aff41f1b40dbffc1aa82158c308ea
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-02-11 20:15:37 -0600

    Info windows

--- public/js/search.js
+++ public/js/search.js
@@ -13,15 +13,30 @@ search.addEventListener('submit', async (e) => {
     const listings = await res.json();
     // Init the map
     let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
+      zoom: 13,
       center: { lat: listings[0].latitude, lng: listings[0].longitude }
   });
+
+  // Create marker for each listing
     listings.forEach((listing) => {
       const marker = new google.maps.Marker({
         position: { lat: listing.latitude, lng: listing.longitude },
         map,
         title: listing.address,
       });
+      
+  // Create the info window for map marker
+  let infoWindow = new google.maps.InfoWindow({
+  content: `
+      <div>${listing.address}</div>
+      <div>${listing.item_name}</div>
+  `
+});
+      // Associate the info window with clicking the marker
+      marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+        });
+
     });
   } catch (err) {
     console.error(err);
